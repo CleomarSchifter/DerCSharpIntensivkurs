2d1c11702e8b48d2e2f46c8582d57e5c49662bf9
Cleomar Schifter <cleomar.schifter.cs@gmail.com>
2023-02-27 13:40:50 +0100

    Update

--- AutoInspection/Program.cs
+++ AutoInspection/Program.cs
@@ -10,6 +10,27 @@ namespace AutoInspection
     {
         static void Main()
         {
+            string[] carNames = new string[6];
+            carNames[0] = "Mercedes G";
+            carNames[1] = "Audi A8";
+            carNames[2] = "BMW 3";
+            carNames[3] = "Audi A6";
+            carNames[4] = "Hyundai";
+            carNames[5] = "Toyota";
+
+            Console.WriteLine("Bitte geben Sie einen Suchbegriff ein: ");
+
+            string searchTerm = Console.ReadLine();
+
+            Console.WriteLine("Suchbegriff ist: " + searchTerm);
+
+            Console.WriteLine("Folgende Autos wurden gefunden: ");
+
+            foreach (string car in carNames)
+            {
+                Console.WriteLine(car);
+            }
+
 
             Console.ReadKey();
             Console.Clear();
