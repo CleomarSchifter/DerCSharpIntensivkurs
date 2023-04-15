6863577d463b876ae7a57d8514266eb04f5f3ab3
Cleomar Schifter <cleomar.schifter.cs@gmail.com>
2023-04-15 07:03:07 +0200

    update

--- AutoInspection/Car.cs
+++ AutoInspection/Car.cs
@@ -11,5 +11,17 @@ namespace AutoInspection
         public string carName = "Audi";
         public int baujahr = 1995;
         public int lastInspectionYear = 2010;
+
+        // Konstruktor
+        public Car (string newName, int newBauJahr)
+        {
+            carName = newName;
+            baujahr = newBauJahr;
+        }
+        // Methoden
+        public void Inspect()
+        {
+            lastInspectionYear = 2023;
+        }
     }
 }
--- AutoInspection/Program.cs
+++ AutoInspection/Program.cs
@@ -11,12 +11,12 @@ namespace AutoInspection
         static void Main()
         {
             Car[] cars = new Car[6];
-            cars[0] = new Car();
-            cars[1] = new Car();
-            cars[2] = new Car();
-            cars[3] = new Car();
-            cars[4] = new Car();
-            cars[5] = new Car();
+            cars[0] = new Car("Mercedes G", 2008);
+            cars[1] = new Car("BMW 3", 2001);
+            cars[2] = new Car("Audi A6", 2005);
+            cars[3] = new Car("Audi A8", 2012);
+            cars[4] = new Car("Hyundai", 2003);
+            cars[5] = new Car("Toyota", 1995);
 
             Console.WriteLine("Bitte geben Sie einen Suchbegriff ein: ");
 
@@ -28,8 +28,13 @@ namespace AutoInspection
 
             foreach (Car car in cars)
             {
-                if(car.carName.Contains(searchTerm))
-                Console.WriteLine(car);
+                if (car.carName.Contains(searchTerm))
+                {
+                    Console.WriteLine(car.carName + " Baujahr: " + car.baujahr + " Letzte Inspektion: " + car.lastInspectionYear);
+                    car.Inspect();
+                    Console.WriteLine("Neues Inspektionsjahr: " + car.lastInspectionYear);
+                }
+                
             }
 
 
